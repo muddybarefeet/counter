a4b188bfe9116b539917437ef9fb343c3bbf8f73
Anna Rogers <2muddybarefeet@gmail.com>
2016-06-27 07:40:45 -0700

    initail commit

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,16 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+//    explicitly unwrap(can be nil and still compile)
+    var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        label
+        label = UILabel()
+        label.frame = CGRectMake(190, 150, 60, 60)
+        label.text = "0"
+        self.view.addSubview(label)
+//        increment button
+        var button = UIButton()
+        button.frame = CGRectMake(150, 250, 100, 60)
+        button.setTitle("Click Me!", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+//        target, method to use and control event
+        button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        
+//        decrement button
+        var decButton = UIButton()
+        decButton.frame = CGRectMake(130, 350, 150, 60)
+        decButton.setTitle("Decrement Button", forState: .Normal)
+        decButton.setTitleColor(UIColor.brownColor(), forState: .Normal)
+        self.view.addSubview(decButton)
+        decButton.addTarget(self, action: #selector(ViewController.decrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func incrementCount () {
+        print("increment clicked")
+        self.count += 1
+        self.label.text = "\(self.count)"
     }
+    
+    func decrementCount () {
+        print("decrement clicked")
+        self.count -= 1
+        self.label.text = "\(self.count)"
+    }
+    
+//    function to toggle the view with each click
+    func toggleBackground () {
+        print("toggle backgournd color")
+//        todo
+    }
+    
+//    additional label to increment with the first?
 
 
 }
